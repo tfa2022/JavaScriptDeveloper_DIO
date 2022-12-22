75c202056fc8caecd323a9b4953d18a360c94e4a
Tiago Freitas <tiagofreitasalves86@gmail.com>
2022-12-21 21:01:51 -0300

    Atualizando Repositorio 2.0

--- javascript-developer-m1/05 - objetos/lista de exercios.js
+++ javascript-developer-m1/05 - objetos/lista de exercios.js
@@ -48,10 +48,29 @@ class pessoa {
         return this.peso / (this.altura * this.altura);
     }
 
+    classificarImc() {
+        const imc = this.calcularImc();
+
+        if (imc < 18.5) {
+            return ("Abaixo do Peso")
+        } else if (imc >= 18.5 && imc < 25) {
+            return ("Peso Normal")
+        } else if (imc >= 25 && imc < 30) {
+            return ("acima do peso")
+        } else if (imc >= 30 && imc < 40) {
+            return ("Obeso")
+        } else {
+            return ("Obesidade Grave")
+        }
+
+    }
+
 }
 
 const jose = new pessoa('jose', 70, 1.75);
 console.log(jose.calcularImc());
+console.log(jose.classificarImc());
 
 const tiago = new pessoa('tiago', 81, 1.68);
 console.log(tiago.calcularImc());
+console.log(tiago.classificarImc());

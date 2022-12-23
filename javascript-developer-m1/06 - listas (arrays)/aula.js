c1accd5b0bb212d95ce51fc9e3f14979ee2344e0
Tiago Freitas <tiagofreitasalves86@gmail.com>
2022-12-22 21:16:17 -0300

    Atualização repositorio

--- javascript-developer-m1/06 - listas (arrays)/aula.js
+++ javascript-developer-m1/06 - listas (arrays)/aula.js
@@ -7,6 +7,8 @@ notas.push(10);
 
 let soma = 0;
 
+//Estrutura de Repetição
+
 for (let i = 0; i < notas.length; i++) {
     const nota = notas[i];
     soma = soma + nota;
@@ -14,3 +16,4 @@ for (let i = 0; i < notas.length; i++) {
 
 const media = soma / notas.length;
 console.log(media);
+
---
+++ javascript-developer-m1/06 - listas (arrays)/lista de exercicio.js
@@ -0,0 +1,56 @@
+
+
+// 1) Crie um programa que dado um número imprima a sua tabuada.
+
+const numero = 5
+
+for (let i = 1; i <= 10; i++) {
+
+    console.log(i * numero);
+}
+
+// 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
+
+const numeros = [2, 5, 7, 10, 12, 19];
+
+for (let i = 0; i < numeros.length; i++) {
+
+    const numero = numeros[i];
+
+    if (numeros % 2 === 0) {
+        console.log(numero);
+    }
+
+}
+
+// 3) Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
+
+const nomes = [vanessa, amanda, julia, victor, tiago];
+
+
+
+
+
+
+
+
+// 4) Faça um programa que preencha uma lista com todos os números pares contidos no intervalo de 10 a 50;
+
+
+
+
+
+
+
+//5) Dado uma lista com as médias tiradas pelos alunos. Imprima todas as médias que ficaram de recuperação (média < 5).
+// Ex de lista = [2, 7, 3, 8, 10, 4]
+
+
+
+
+
+
+
+
+// 6) Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.
+// Ex de lista de notas = [2, 7, 3, 8, 10, 4]
